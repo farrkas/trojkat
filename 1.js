6cee356b56fa85acf32e54026cf9671f00b1a67e
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-02-02 23:24:07 +0100

    trojkat

---
+++ 1.js
@@ -0,0 +1,14 @@
+function isTriangle(a,b,c){
+    if (a<=0 || b<=0 ||c<=0){
+        return false;
+                    }
+    else {
+        if (a+b>c && a+c>b && b+c>a ){
+            return true;
+            
+        }
+        else {
+            return false;
+        }
+    } 
+}
